4ed3bf2c945e74231de943bb1147af6a746f4f2e
Phong Nguyen <phongnt.ptnk@gmail.com>
2017-10-14 23:14:42 +0800

    copied parsing answer to continue

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,107 +1,185 @@
 package fpinscala.parsing
 
-import fpinscala.parsing.Parsers.Parser
-import fpinscala.testing._
-
-import language.{higherKinds, implicitConversions}
+import java.util.regex._
 import scala.util.matching.Regex
-
-sealed trait Result[+A] {
-  def mapError(f: ParseError => ParseError): Result[A] = this match {
-    case Failure(e) => Failure(f(e))
-    case _ => this
-  }
-}
-case class Success[+A](get: A, charsConsumed: Int) extends Result[A]
-case class Failure(get: ParseError) extends Result[Nothing]
-object Parsers {
-  type Parser[+A] = Location => Result[A]
-}
+import fpinscala.testing._
+import fpinscala.testing.Prop._
+import language.higherKinds
+import language.implicitConversions
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+  def run[A](p: Parser[A])(input: String): Either[ParseError,A]
 
   implicit def string(s: String): Parser[String]
-  implicit def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
-  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
-  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
-  implicit def regex(r: Regex): Parser[String]
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+    ParserOps[String] = ParserOps(f(a))
 
-  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def attempt[A](p: Parser[A]): Parser[A]
-  def label[A](msg: String)(p: Parser[A]): Parser[A]
-  def scope[A](msg: String)(p: Parser[A]): Parser[A]
-  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
-  //Exercise 9.4
+  def char(c: Char): Parser[Char] =
+    string(c.toString) map (_.charAt(0))
+
+  /*
+   * A default `succeed` implementation in terms of `string` and `map`.
+   * We leave `succeed` abstract, since `map` is defined below in terms of
+   * `flatMap` and `succeed`, which would be a circular definition! But we include
+   * the definition here in case implementations wish to use it
+   * (say if they provide a custom implementation of `map`, breaking the cycle)
+   */
+  def defaultSucceed[A](a: A): Parser[A] =
+    string("") map (_ => a)
+
+  def succeed[A](a: A): Parser[A]
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _)
+
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
     if (n <= 0) succeed(List())
     else map2(p, listOfN(n-1, p))(_ :: _)
 
-  //Exercise 9.3
   def many[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _) or succeed(List())
 
-  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
 
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
-    flatMap(p)(a => succeed(f(a)))
+  def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
-  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+  implicit def regex(r: Regex): Parser[String]
+
+  /*
+  These can be implemented using a for-comprehension, which delegates to the `flatMap` and `map` implementations we've provided on `ParserOps`, or they can be implemented in terms of these functions directly.
+  */
+  def product[A,B](p: Parser[A], p2: => Parser[B]): Parser[(A,B)] =
     flatMap(p)(a => map(p2)(b => (a,b)))
 
-  def slice[A](p: Parser[A]): Parser[String]
+  def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] =
+    for { a <- p; b <- p2 } yield f(a,b)
 
-  val numA: Parser[Int] = char('a').many.map(_.size)
-  val aNums: Parser[Int] = for {
-    digit <- "[0-9]".r
-    val n = digit.toInt
-    _ <- listOfN(n, char('a'))
-  } yield n
+  def map[A,B](a: Parser[A])(f: A => B): Parser[B] =
+    flatMap(a)(f andThen succeed)
 
-  //Exercise 9.1
-  // product is primitive, implementing map2 in term on product
-  def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
-//    (p ** p2).map(f.tupled)
-  flatMap(p)(a => map(p2)(b => f(a,b)))
+  def label[A](msg: String)(p: Parser[A]): Parser[A]
 
+  def scope[A](msg: String)(p: Parser[A]): Parser[A]
 
-  def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p).map(_.drop(1))) { _ :: _}
+  def attempt[A](p: Parser[A]): Parser[A]
 
-  def errorLocation(e: ParseError): Location
-  def errorMessage(e: ParseError): String
+  /** Sequences two parsers, ignoring the result of the first.
+    * We wrap the ignored half in slice, since we don't care about its result. */
+  def skipL[B](p: Parser[Any], p2: => Parser[B]): Parser[B] =
+    map2(slice(p), p2)((_,b) => b)
+
+  /** Sequences two parsers, ignoring the result of the second.
+    * We wrap the ignored half in slice, since we don't care about its result. */
+  def skipR[A](p: Parser[A], p2: => Parser[Any]): Parser[A] =
+    map2(p, slice(p2))((a,b) => a)
+
+  def opt[A](p: Parser[A]): Parser[Option[A]] =
+    p.map(Some(_)) or succeed(None)
+
+  /** Parser which consumes zero or more whitespace characters. */
+  def whitespace: Parser[String] = "\\s*".r
+
+  /** Parser which consumes 1 or more digits. */
+  def digits: Parser[String] = "\\d+".r
+
+  /** Parser which consumes reluctantly until it encounters the given string. */
+  def thru(s: String): Parser[String] = (".*?"+Pattern.quote(s)).r
+
+  /** Unescaped string literals, like "foo" or "bar". */
+  def quoted: Parser[String] = string("\"") *> thru("\"").map(_.dropRight(1))
+
+  /** Unescaped or escaped string literals, like "An \n important \"Quotation\"" or "bar". */
+  def escapedQuoted: Parser[String] =
+    // rather annoying to write, left as an exercise
+    // we'll just use quoted (unescaped literals) for now
+    token(quoted label "string literal")
+
+  /** C/Java style floating point literals, e.g .1, -1.0, 1e9, 1E-23, etc.
+    * Result is left as a string to keep full precision
+    */
+  def doubleString: Parser[String] =
+    token("[-+]?([0-9]*\\.)?[0-9]+([eE][-+]?[0-9]+)?".r)
+
+  /** Floating point literals, converted to a `Double`. */
+  def double: Parser[Double] =
+    doubleString map (_.toDouble) label "double literal"
+
+  /** Attempts `p` and strips trailing whitespace, usually used for the tokens of a grammar. */
+  def token[A](p: Parser[A]): Parser[A] =
+    attempt(p) <* whitespace
+
+  /** Zero or more repetitions of `p`, separated by `p2`, whose results are ignored. */
+  def sep[A](p: Parser[A], p2: Parser[Any]): Parser[List[A]] = // use `Parser[Any]` since don't care about result type of separator
+    sep1(p,p2) or succeed(List())
+
+  /** One or more repetitions of `p`, separated by `p2`, whose results are ignored. */
+  def sep1[A](p: Parser[A], p2: Parser[Any]): Parser[List[A]] =
+    map2(p, many(p2 *> p))(_ :: _)
+
+  /** Parses a sequence of left-associative binary operators with the same precedence. */
+  def opL[A](p: Parser[A])(op: Parser[(A,A) => A]): Parser[A] =
+    map2(p, many(op ** p))((h,t) => t.foldLeft(h)((a,b) => b._1(a,b._2)))
+
+  /** Wraps `p` in start/stop delimiters. */
+  def surround[A](start: Parser[Any], stop: Parser[Any])(p: => Parser[A]) =
+    start *> p <* stop
+
+  /** A parser that succeeds when given empty input. */
+  def eof: Parser[String] =
+    regex("\\z".r).label("unexpected trailing characters")
+
+  /** The root of the grammar, expects no further input following `p`. */
+  def root[A](p: Parser[A]): Parser[A] =
+    p <* eof
+
   case class ParserOps[A](p: Parser[A]) {
-    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
-    def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
-    def **[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
-    def map[B](f: A => B): Parser[B] = self.map(p)(f)
-    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
-    def many: Parser[List[A]] = self.many(p) }
+    def |[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2) // use `self` to explicitly disambiguate reference to the `or` method on the `trait`
+    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
 
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def many = self.many(p)
+
+    def slice: Parser[String] = self.slice(p)
+
+    def **[B](p2: => Parser[B]): Parser[(A,B)] =
+      self.product(p,p2)
+    def product[B](p2: => Parser[B]): Parser[(A,B)] =
+      self.product(p,p2)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] =
+      self.flatMap(p)(f)
+
+    def label(msg: String): Parser[A] = self.label(msg)(p)
+
+    def scope(msg: String): Parser[A] = self.scope(msg)(p)
+
+    def *>[B](p2: => Parser[B]) = self.skipL(p, p2)
+    def <*(p2: => Parser[Any]) = self.skipR(p, p2)
+    def token = self.token(p)
+    def sep(separator: Parser[Any]) = self.sep(p, separator)
+    def sep1(separator: Parser[Any]) = self.sep1(p, separator)
+    def as[B](b: B): Parser[B] = self.map(self.slice(p))(_ => b)
+    def opL(op: Parser[(A,A) => A]): Parser[A] = self.opL(p)(op)
+  }
   object Laws {
-    import Prop.forAll
     def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
       forAll(in)(s => run(p1)(s) == run(p2)(s))
 
     def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
       equal(p, p.map(a => a))(in)
-
-    def succeedLaw[A](a: A)(in: Gen[String]): Prop = forAll(in)(s => run(succeed(a))(s) == Right(a))
-
-    def labelLaw[A](p: Parser[A], inputs: SGen[String]): Prop =
-      forAll(inputs ** Gen.string) { case (input, msg) =>
-          run(label(msg)(p))(input) match {
-            case Left(e) => errorMessage(e) == msg
-            case _ => true
-          }
-
-      }
   }
 }
 
 case class Location(input: String, offset: Int = 0) {
 
   lazy val line = input.slice(0,offset+1).count(_ == '\n') + 1
-  lazy val col = input.slice(0,offset+1).reverse.indexOf('\n')
+  lazy val col = input.slice(0,offset+1).lastIndexOf('\n') match {
+    case -1 => offset + 1
+    case lineStart => offset - lineStart
+  }
 
   def toError(msg: String): ParseError =
     ParseError(List((this, msg)))
@@ -109,58 +187,61 @@ case class Location(input: String, offset: Int = 0) {
   def advanceBy(n: Int) = copy(offset = offset+n)
 
   /* Returns the line corresponding to this location */
-  def currentLine: String = 
+  def currentLine: String =
     if (input.length > 1) input.lines.drop(line-1).next
     else ""
+
+  def columnCaret = (" " * (col-1)) + "^"
 }
 
-case class ParseError(stack: List[(Location,String)] = List(),
-                      otherFailures: List[ParseError] = List()) {
+case class ParseError(stack: List[(Location,String)] = List()) {
   def push(loc: Location, msg: String): ParseError =
-    copy(stack = (loc, msg) :: stack)
+    copy(stack = (loc,msg) :: stack)
 
   def label[A](s: String): ParseError =
     ParseError(latestLoc.map((_,s)).toList)
 
+  def latest: Option[(Location,String)] =
+    stack.lastOption
+
   def latestLoc: Option[Location] =
     latest map (_._1)
 
-  def latest: Option[(Location,String)] =
-    stack.lastOption
+  /**
+  Display collapsed error stack - any adjacent stack elements with the
+  same location are combined on one line. For the bottommost error, we
+  display the full line, with a caret pointing to the column of the error.
+  Example:
+
+  1.1 file 'companies.json'; array
+  5.1 object
+  5.2 key-value
+  5.10 ':'
+
+  { "MSFT" ; 24,
+  */
+  override def toString =
+    if (stack.isEmpty) "no error message"
+    else {
+      val collapsed = collapseStack(stack)
+      val context =
+        collapsed.lastOption.map("\n\n" + _._1.currentLine).getOrElse("") +
+        collapsed.lastOption.map("\n" + _._1.columnCaret).getOrElse("")
+      collapsed.map { case (loc,msg) => loc.line.toString + "." + loc.col + " " + msg }.mkString("\n") +
+      context
+    }
+
+  /* Builds a collapsed version of the given error stack -
+   * messages at the same location have their messages merged,
+   * separated by semicolons */
+  def collapseStack(s: List[(Location,String)]): List[(Location,String)] =
+    s.groupBy(_._1).
+      mapValues(_.map(_._2).mkString("; ")).
+      toList.sortBy(_._1.offset)
+
+  def formatLoc(l: Location): String = l.line + "." + l.col
 }
 
-object MyParser extends Parsers[Parser] {
-  override implicit def string(s: String): Parser[String] = ???
+object Parsers {
 
-  override implicit def regex(r: Regex): Parser[String] = ???
-
-  override def run[A](p: Parser[A])(input: String): Either[ParseError, A] = ???
-
-  override def attempt[A](p: Parser[A]): Parser[A] = ???
-
-  override def label[A](msg: String)(p: Parser[A]): Parser[A] =
-    s => p(s).mapError(_.la)
-
-  override def scope[A](msg: String)(p: Parser[A]): Parser[A] =
-    s => p(s).mapError(_.push(s, msg))
-
-  override def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A] = ???
-
-  override def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B] = ???
-
-  override def slice[A](p: Parser[A]): Parser[String] = ???
-
-  override def errorLocation(e: ParseError): Location = ???
-
-  override def errorMessage(e: ParseError): String = ???
-}
-
-trait JSON
-object JSON {
-  case object JNull extends JSON
-  case class JNumber(get: Double) extends JSON
-  case class JString(get: String) extends JSON
-  case class JBool(get: Boolean) extends JSON
-  case class JArray(get: IndexedSeq[JSON]) extends JSON
-  case class JObject(get: Map[String, JSON]) extends JSON
 }
